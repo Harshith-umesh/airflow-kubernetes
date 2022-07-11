e05b0c1c6d5364afba21a404363986057d0c16bd
Harshith-umesh <harshith.umesh.nat@gmail.com>
2022-07-12 00:24:18 +0530

    testing

--- dags/openshift_nightlies/scripts/run_benchmark.sh
+++ dags/openshift_nightlies/scripts/run_benchmark.sh
@@ -88,7 +88,7 @@ else
 
     if [[ ${benchmark_rv} -eq 0 && ${workload} == "kube-burner" && ${GIT_USER} == "harshith-umesh" ]] ; then 
         export PGPASSWORD=${POSTGRES_PASSWORD} 
-        psql -h ${POSTGRES_SERVER_URL} -U ${POSTGRES_USERNAME} -d ${POSTGRES_DATABASE} -c "insert into ci_runs values('${UUID}',False);" 2>/dev/null
+        psql -h ${POSTGRES_SERVER_URL} -U ${POSTGRES_USERNAME} -d ${POSTGRES_DATABASE} -c "insert into ci_runs values('${UUID}',False);" 
     fi
 
 
